3b2c9f66233fc093baf08e076b0724b9826b92d5
Kelvinn XTkj2 <144640412+YonkouNoPinn@users.noreply.github.com>
2023-09-11 09:32:30 +0700

    Create Luassegitiga.cpp

---
+++ Luassegitiga.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+int a,t,l;
+cout<<"MENGHITUNG LUAS SEGITIGA \n";
+cout<<"masukkan alas: ";
+cin>>a;
+cout<<"masukkan tinggi: ";
+cin>>t;
+l=0.5*a*t;
+cout<<"hasilnya adalah: "<<l<<endl;
+return 0;
+}
